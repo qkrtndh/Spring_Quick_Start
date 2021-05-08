2d2a3318ac1c9ac8b55b9de97ba13984af2d1afb
qkrtndh <qkrtndh5@naver.com>
2021-05-08 14:44:06 +0900

    1.4.3 디자인 패턴 이용하기

--- src/main/java/com/springbook/biz/TVUser.java
+++ src/main/java/com/springbook/biz/TVUser.java
@@ -3,13 +3,14 @@ package com.springbook.biz;
 public class TVUser {
 
 	public static void main(String[] args) {
-		//형변환
-		TV tv = new SamsungTV();
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV)factory.getBean(args[0]);
+			
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
 
 	}
-
+//실행시 run config~~ 아규먼트윗 상자칸에 lg나 samsung 입력
 }
