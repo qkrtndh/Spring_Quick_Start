2cc98bf953f9e6991e8c54cbaaf27994a518e2f9
qkrtndh <qkrtndh5@naver.com>
2021-05-08 19:27:08 +0900

    3.2.1 다중변수 매핑

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -2,6 +2,7 @@ package com.springbook.biz;
 
 public class SamsungTV implements TV {
 	private SonySpeaker speaker;
+	private int price;
 	public SamsungTV() {
 		System.out.println("===> SamsungTV(1) 객체 생성");
 	}
@@ -9,6 +10,12 @@ public class SamsungTV implements TV {
 		System.out.println("===> SamsungTV(2) 객체 생성");
 		this.speaker=speaker;
 	}
+	public SamsungTV(SonySpeaker speaker,int price) {
+		System.out.println("===> SamsungTV(3) 객체 생성");
+		this.speaker=speaker;
+		this.price=price;
+	}
+	
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리");
 	}
@@ -16,7 +23,7 @@ public class SamsungTV implements TV {
 		System.out.println("객체 삭제 전 처리할 로직 처리...");
 	}
 	public void powerOn() {
-		System.out.println("SamsungTV---전원킴.");
+		System.out.println("SamsungTV---전원킴.(가격: "+price+")");
 	}
 	public void powerOff() {
 		System.out.println("SamsungTV---전원끔.");
