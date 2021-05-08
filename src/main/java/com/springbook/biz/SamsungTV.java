09350c9e7cbcac3ef890a96dd003ac43633e4568
qkrtndh <qkrtndh5@naver.com>
2021-05-08 16:41:33 +0900

    ~ 2.2.4.1 init-Method

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -4,6 +4,10 @@ public class SamsungTV implements TV {
 	public SamsungTV() {
 		System.out.println("===> SamsungTV 객체 생성");
 	}
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리");
+	}
+	
 	public void powerOn() {
 		System.out.println("SamsungTV---전원킴.");
 	}
