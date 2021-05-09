6317d95ef8ceef104d87cd3e75ddd26037bca275
qkrtndh <qkrtndh5@naver.com>
2021-05-09 11:18:40 +0900

    3.3.1 Setter 인젝션

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -6,13 +6,12 @@ public class SamsungTV implements TV {
 	public SamsungTV() {
 		System.out.println("===> SamsungTV(1) 객체 생성");
 	}
-	public SamsungTV(Speaker speaker) {
-		System.out.println("===> SamsungTV(2) 객체 생성");
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker 호출");
 		this.speaker=speaker;
 	}
-	public SamsungTV(Speaker speaker,int price) {
-		System.out.println("===> SamsungTV(3) 객체 생성");
-		this.speaker=speaker;
+	public void setPrice(int price) {
+		System.out.println("===> setPrice 호출");
 		this.price=price;
 	}
 	
