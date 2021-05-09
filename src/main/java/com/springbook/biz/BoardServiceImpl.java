629513cc9f2e56261fdb197c199b24ff38a07f10
qkrtndh <qkrtndh5@naver.com>
2021-05-09 16:54:44 +0900

    5.6 BoardService 컴포넌트 테스트 - 코드수정 필요

--- src/main/java/com/springbook/biz/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/BoardServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("boardService")
-public class BoardServiceImpl {
+public class BoardServiceImpl implements BoardService{
 	@Autowired
 	private BoardDAO boardDAO;
 	
