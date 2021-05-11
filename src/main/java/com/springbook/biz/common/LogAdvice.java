49f10cde69ae68555b1fa7668fca32c48479556f
qkrtndh <qkrtndh5@naver.com>
2021-05-11 16:05:26 +0900

    5.1.3 어드바이스 설정

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,13 +1,20 @@
 package com.springbook.biz.common;
 
 import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogAdvice {
 	@Pointcut ("execution(* com.springbook.biz..*Impl.*(..))")
 	public void allPointcut() {}
+	
+	@Before("allPointcut()")
+	public void printLLog() {
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	}
 	
 	@Pointcut ("execution(* com.springbook.biz..*Impl.get*(..))")
 	public void getPointcut() {}
