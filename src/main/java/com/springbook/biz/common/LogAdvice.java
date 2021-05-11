9ea92a13acf67bf18106d154995829dfc5524292
qkrtndh <qkrtndh5@naver.com>
2021-05-11 16:18:13 +0900

    5.1.4 애스팩트 설정

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,21 +1,25 @@
 package com.springbook.biz.common;
 
 import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
+@Aspect //Aspect = Pointcut + Advice
 public class LogAdvice {
+	
+	//Pointcut
 	@Pointcut ("execution(* com.springbook.biz..*Impl.*(..))")
 	public void allPointcut() {}
 	
+	//Advice
 	@Before("allPointcut()")
 	public void printLLog() {
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
 	
-	@Pointcut ("execution(* com.springbook.biz..*Impl.get*(..))")
-	public void getPointcut() {}
+	
 }
