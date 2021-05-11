3993dce2825e8a3cd1e021994043c38333c1d047
qkrtndh <qkrtndh5@naver.com>
2021-05-11 15:58:05 +0900

    5.1.2 포인트컷 설정

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,9 +1,14 @@
 package com.springbook.biz.common;
 
 import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LogAdvice {
-	public void printLog(Joinpoint jp) {
-		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
-	}
+	@Pointcut ("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Pointcut ("execution(* com.springbook.biz..*Impl.get*(..))")
+	public void getPointcut() {}
 }
